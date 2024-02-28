a262de1f4e5a95db06d6bbbeb0e3887a5de5fe05
Vaibhav Raj <113601669+vavraj@users.noreply.github.com>
2024-02-29 01:55:20 +0530

    Time: 14 ms (51.29%), Space: 23.3 MB (7.69%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    
-    int solve(vector<int>&satisfaction,int i,int time,vector<vector<int> >&dp){
-        if(i==satisfaction.size())
-        {
-            return 0;
+   
+    int solveTab(vector<int>&satisfaction){
+        int n=satisfaction.size();
+        vector<vector<int> >dp(n+1,vector<int>(n+1,0));
+        
+        for(int index=n-1;index>=0;index--){
+            for(int time=index;time>=0;time--){
+                int include=                                    ((time+1)*satisfaction[index])+dp[index+1][time+1];
+        int exclude=0+dp[index+1][time];
+        
+        dp[index][time]=max(include,exclude);
+            }
+            
         }
-        if(dp[i][time]!=-1)
-            return dp[i][time];
         
-        int include=((time+1)*satisfaction[i])+solve(satisfaction,i+1,time+1,dp);
-        int exclude=0+solve(satisfaction,i+1,time,dp);
-        
-        dp[i][time]=max(include,exclude);
-        return dp[i][time];
+        return dp[0][0];
     }
     int maxSatisfaction(vector<int>& satisfaction) {
         
         sort(satisfaction.begin(),satisfaction.end());
         
-        vector<vector<int> >dp(satisfaction.size()+1,vector<int>(satisfaction.size()+1,-1));
-        return solve(satisfaction,0,0,dp);
+        return solveTab(satisfaction);
     }
 };
