53e5fca7ea3f084926b21477c7b3c58f7dda263d
Vaibhav Raj <113601669+vavraj@users.noreply.github.com>
2024-06-28 02:29:50 +0530

    Time: 11 ms (46.88%), Space: 7 MB (71.79%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+        
+        int solve(int n){
+                if(n==0)
+                        return 0;
+                if(n==1)
+                        return 1;
+                
+                return solve(n-1)+solve(n-2);
+        }
+    int fib(int n) {
+        return solve(n);
+    }
+};
